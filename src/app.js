3d6ec168a1533c4948f026177ceab3b75e38ec38
KaunajB <kolkatachamps@jubi.ai>
2020-08-24 15:08:27 +0530

    msg

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 let server = require('http').Server(app);
-let io = require('socket.io')(server);
+let io = require('socket.io')(server, { path: '/stream' });
 let stream = require('./ws/stream');
 let path = require('path');
 let favicon = require('serve-favicon');
@@ -16,6 +16,7 @@ app.get('/', (req, res) => {
 });
 
 
-io.of('/video-call/stream').on('connection', stream);
+// io.of('/video-call/stream').on('connection', stream);
+io.sockets.on('connection', stream);
 
 server.listen(port, () => console.log('Server listening on port', port));
